a0e621572f21a33a08dfe385a3a61c77011a2305
jwlsiu <114306901+jwlsiu@users.noreply.github.com>
2023-04-16 16:21:00 +0100

    Add files via upload

---
+++ vanillaJSBasics.js
@@ -0,0 +1,29 @@
+// Vanilla JS Basics - Activity 1
+
+// Declare each variable and print result
+
+let a = -5 + 8 * 6 
+console.log(a);
+
+let b = (55 + 9) % 9 
+console.log(b);
+
+let c = 20 + -3 * 5 / 8 
+console.log(c);
+
+let d = 5 + 15 / 3 * 2 - 8 % 3
+console.log(d);
+
+
+// Vanilla JS Basics - Activity 2
+
+// Declare variables for first and second number user input
+let first_num = prompt('Enter first number: ')
+let second_num = prompt('Enter second number: ')
+
+//Declare result variable to work out first number divided by the second number
+let result = first_num / second_num 
+
+// Print sentence and result in console and as an alert
+console.log(`The division of the first number and the second number is: ${result}`)
+alert(`The division of the first number and the second number is: ${result}`);
